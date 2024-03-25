addf4e3d76c60414f3c3cf392923dbce2ad50671
Wasif Mahmood <116950247+wasifmahmood@users.noreply.github.com>
2024-03-25 21:09:10 +0500

    navigate add

--- charlie/app/Components/HeaderDrawer.js
+++ charlie/app/Components/HeaderDrawer.js
@@ -2,8 +2,23 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 import Divider from "@mui/material/Divider";
+import About from "../pages/About/About";
+import Location from "../pages/Location/Location";
+import Service from "../pages/Service/Service";
+import { useNavigate } from "react-router-dom";
 
 export const HeaderDrawer = ({ isOpen, onClose }) => {
+    // const navigate = useNavigate();
+
+    const aboutClick = () => {
+        navigate('/About');
+    };
+    const locationClick = () => {
+        navigate('/Location');
+    };
+    const serviceClick = () => {
+        navigate('/Service');
+    };
     const list = (
         <Box
             sx={{ width: 250 }}
@@ -13,9 +28,9 @@ export const HeaderDrawer = ({ isOpen, onClose }) => {
         >
             <Divider />
             <div className="text-center pt-5 text-lg text-wrap">
-                <h5>About</h5>
-                <h5>Location</h5>
-                <h5>Services</h5>
+                <h5 onClick={aboutClick}>About</h5>
+                <h5 onClick={locationClick}>Location</h5>
+                <h5 onClick={serviceClick}>Services</h5>
                 <h5>Contact</h5>
             </div>
         </Box>
